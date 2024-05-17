5c0a16c02287b93c5b37aa3b881050d1e7aed2a2
ksg-dev <ksg.dev.data@gmail.com>
2024-05-17 12:03:03 -0500

    random walk

---
+++ walk.py
@@ -0,0 +1,33 @@
+from turtle import Turtle, Screen
+import random
+
+tes = Turtle()
+my_screen = Screen()
+#tes.shape("turtle")
+my_screen.colormode(255)
+
+# Correct pen settings to use on final run
+tes.speed(10)
+tes.shape("circle")
+tes.pensize(10)
+tes.shapesize(0.1, 0.1)
+
+
+def get_color():
+    c1 = random.randint(1, 256)
+    c2 = random.randint(1, 256)
+    c3 = random.randint(1, 256)
+    return c1, c2, c3
+
+
+directions = [0, 90, 180, 270]
+
+
+for _ in range(200):
+    direction = random.choice(directions)
+    tes.pencolor(get_color())
+    tes.setheading(direction)
+    tes.forward(50)
+
+
+my_screen.exitonclick()
